01fe8bf9c4e5709ed217680de78b924348964437
Iyaddiyaeldin <iyadaddiya@gmail.com>
2025-07-09 01:53:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+const puppeteer = require('puppeteer');
+const express = require('express');
+
+const USERNAME = 'bothelper123';
+const PASSWORD = 'iyad2009*iyad';
+const SERVER_SLUG = 'ourserver-5LOn';
+
+async function startAternosServer() {
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--]()
 
